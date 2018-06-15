a295cc8c1fe20feab33a84abb7888040ebffb09b
Naresh yadav <Naresh yadav@192.168.0.55>
2018-06-15 16:57:03 +0530

    2nd modification sathyaDev1

--- DateProj/src/com/nt/test/DateTest.java
+++ DateProj/src/com/nt/test/DateTest.java
@@ -2,11 +2,20 @@ package com.nt.test;
 
 import java.util.Date;
 
+import com.nt.domain.Employee;
+
 public class DateTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Employee e=new Employee();
+		e.setEid(101);
+		e.setEname("sathya");
+		e.setSalary(100000);
      System.out.println("my GITHUB project created on:  "+new Date());
+     System.out.println(e.getEid());
+     System.out.println(e.getEname());
+     System.out.println(e.getSalary());
 	}
 
 }
