8d9d8d56546c010b0c9ff4dd2a68702d287c6331
Naresh yadav <Naresh yadav@192.168.0.55>
2018-06-15 16:42:56 +0530

    AddTL1

---
+++ DateProj/src/com/nt/test/DateTest.java
@@ -0,0 +1,12 @@
+package com.nt.test;
+
+import java.util.Date;
+
+public class DateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("my GITHUB project created on:  "+new Date());
+	}
+
+}
